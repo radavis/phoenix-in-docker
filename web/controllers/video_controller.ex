137dc5c651118e14dd5f1b76e169b1a769ca579c
Richard Davis <rdavis.bacs@gmail.com>
2021-03-16 14:21:56 -0400

    associate user with video

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -9,7 +9,9 @@ defmodule Rumbl.VideoController do
   end
 
   def new(conn, _params) do
-    changeset = Video.changeset(%Video{})
+    changeset = conn.assigns.current_user
+    |> build_assoc(:videos)
+    |> Video.changeset()
     render(conn, "new.html", changeset: changeset)
   end
 
