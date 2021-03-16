11a476c6f316b41a7fc8095bb985887438c90606
Richard Davis <rdavis.bacs@gmail.com>
2021-03-16 13:39:31 -0400

    generate video scaffold, move authenticate_user

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Rumbl
 
+## Generating Resources
+
+* `phoenix.gen.html` - HTML scaffold
+* `phoenix.gen.json` - JSON scaffold
+
+generate a video resource
+
+```bash
+$ dc run app \
+    mix phoenix.gen.html Video videos \
+      user_id:references:users \
+      url:string \
+      title:string \
+      description:text
+$ dc run app \
+    mix ecto.migrate
+```
--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.Auth do
-  import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Plug.Conn
+  import Phoenix.Controller
+  alias Rumbl.Router.Helpers
 
   def init(opts) do
     Keyword.fetch!(opts, :repo)
@@ -37,5 +39,16 @@ defmodule Rumbl.Auth do
   def logout(conn) do
     configure_session(conn, drop: true)  # destroy the session, or
     # delete_session(conn, :user_id)  # to delete only the user_id
+  end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
